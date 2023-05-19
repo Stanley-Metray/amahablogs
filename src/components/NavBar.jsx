ccc97e207edd271f42ff495ab1189bff147a0800
Stanley Metray <stanleymetray@gmail.com>
2023-05-19 17:15:54 +0530

    input issue resolved

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const NavBar = (props) => {
     const [display, setDisplay] = useState();
     const [justifyContent, setJustifyContent] = useState();
+    const [value, setValue] = useState("");
 
     const changeHandler = (e) => {
         let val = e.target.value;
@@ -15,7 +16,7 @@ const NavBar = (props) => {
             setJustifyContent("justify-content-between");
         }
         props.getSearchTitle(val);
-
+        setValue(val);
     }
 
     return <>
@@ -23,7 +24,7 @@ const NavBar = (props) => {
             <div className={`container-fluid ${justifyContent}`}>
                 <h4 style={{ display: display }} className="text-dark content-title">All articles</h4>
                 <form id="form-search" role="search" autoComplete="off">
-                    <input value="" onChange={changeHandler} id="search" className="p-2" type="text" placeholder="Search articles" aria-label="Search" />
+                    <input value={value} onChange={changeHandler} id="search" className="p-2" type="text" placeholder="Search articles" aria-label="Search" />
                     <i className="bi bi-search"></i>
                 </form>
             </div>
